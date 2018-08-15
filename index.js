d4871769dccb6fc1053cd5cad4f01ba9596fa37d
ekoheryanto <eko.heryanto94@gmail.com>
2018-08-15 20:30:16 +0100

    Add fungsi Variabel

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var messege = "Hello Coder";
 let msg1 = "Selamat Belajar JS";
+
 const msg2 = "Frontend Dev";
+if(true){
+  var messege = "Hello dev";
+  let msg1 = "Variabel let Lokal di Satu Tempat";
+  console.log(msg1)
+}
+console.log(messege)
 console.log(msg1)

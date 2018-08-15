860db65b177847b0b51ce7a76e363b52703a5e67
ekoheryanto <eko.heryanto94@gmail.com>
2018-08-15 20:46:09 +0100

    add file function & penulisan

---
+++ function.js
@@ -0,0 +1,12 @@
+function arahpertama() {
+  console.log("Cara Pertama")
+}
+arahpertama() //memanggil function
+const arahkedua = function(){   //disarankan memakai variabel const agar tidak berubah
+  console.log("Cara Kedua")
+}
+arahkedua()
+const arahbaru = ()=>{
+  console.log("Cara Baru")
+}
+arahbaru()

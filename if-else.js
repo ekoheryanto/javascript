18c49aaebec4293ab22a1b013615d4305123f70a
ekoheryanto <eko.heryanto94@gmail.com>
2018-08-20 21:12:33 +0100

    add file if-else.js & add file switch.js

---
+++ if-else.js
@@ -0,0 +1,11 @@
+function arahsatu(){
+  var nilai = 2;
+  // if(nilai === 4){
+  //   console.log("Betul Sangat")
+  // }else {
+  //   console.log("Salah Bingit")
+  // }
+  if(nilai===2)console.log("OK")
+  else console.log("NO")
+}
+arahsatu()
---
+++ switch.js
@@ -0,0 +1,18 @@
+function cobaswitch(){
+  var nilai=4;
+
+  switch(nilai){
+    case 10:
+      console.log("Bagus Bangeut");
+      break;
+    case 8:
+      console.log("Bagus");
+      break;
+    case 6:
+      console.log("Cekap");
+      break;
+    default:
+    console.log("Lainnya");
+  }
+}
+cobaswitch()

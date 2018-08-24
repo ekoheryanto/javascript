576f03af352d1eaf85bc794538aabcb7f7812553
ekoheryanto <eko.heryanto94@gmail.com>
2018-08-24 19:20:41 +0100

    add file for.js & array.js

---
+++ array.js
@@ -0,0 +1,10 @@
+function aray(){
+var data = [1,2,3,4,5,6,7,8,9,10,11];
+var nami = ["asep","udin","komar","aden"];
+
+console.log(data.length);
+console.log(nami.length);
+
+console.log(nami[3]);
+}
+aray()

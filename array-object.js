915a7c89a07652223c204f9a2084f51b7703b27c
ekoheryanto <eko.heryanto94@gmail.com>
2018-08-29 21:04:28 +0100

    add object.js & array-object.js

---
+++ array-object.js
@@ -0,0 +1,7 @@
+function arrayinobjek(){
+var motor = {type:'Honda',harga:12000,warna:'hitam',tahun:[2010,2012,2013,2018]};
+
+console.log(motor.tahun[2]);
+}
+
+arrayinobjek();
---
+++ object.js
@@ -0,0 +1,10 @@
+function objek(){
+
+var motor = {type:'Honda',harga:12000,warna:'hitam'};
+
+console.log(motor)
+console.log(motor.type)
+console.log(motor.warna)
+
+}
+objek();

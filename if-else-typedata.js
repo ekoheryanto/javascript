3759758e4918707a98e8178b3bcce9295948e404
ekoheryanto <eko.heryanto94@gmail.com>
2018-09-10 20:22:51 +0100

    add file if-else-typedata.js

---
+++ if-else-typedata.js
@@ -0,0 +1,13 @@
+function ifelsetype() {
+
+var data = 'Cianjur';
+console.log(typeof(data));
+
+if(data === 'Cianjur'){
+  return 'Muhun eta'
+}else{
+  return 'Sanes eta'
+}
+
+}
+console.log(ifelsetype());

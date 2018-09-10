62f70e4386dac82e213770cf9b3560696774f5ca
ekoheryanto <eko.heryanto94@gmail.com>
2018-09-10 20:49:32 +0100

    add file array-push & array-shift

---
+++ array-push.js
@@ -0,0 +1,7 @@
+function callpush() {
+var data=[1,2,3,4,5,6,7,8,9];
+console.log(data);
+data.push(10);
+return data
+}
+console.log(callpush());
---
+++ array-shift.js
@@ -0,0 +1,11 @@
+function shiftarray() {
+  var kota =['cianjur','bandung','tasikmalaya','jogja'];
+  console.log(kota);
+  console.log('///////////////////////////////////////////////////')
+  // kota =kota.shift();
+  var kota2 = kota.shift();
+  console.log(kota2);
+
+  return kota
+}
+console.log(shiftarray());

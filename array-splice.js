a330d0cafc0173e2c50b958b9405f7f7dd80a444
ekoheryanto <eko.heryanto94@gmail.com>
2018-09-15 19:30:44 +0100

    edit file array-splice.js fungsi delete data array

--- array-splice.js
+++ array-splice.js
@@ -1,7 +1,8 @@
 function callsplice() {
 var kota=['jakarta','medan','malang','cianjur'];
 console.log(kota)
-kota.splice(2,0,'bandung');
+//kota.splice(2,0,'bandung');    //tambah penyisipan data
+kota.splice(3,1); //penghapusan data array
 return kota;
 }
 console.log(callsplice());

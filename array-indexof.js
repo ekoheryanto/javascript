29c70a7b75ce069abf4df73b596c6ce34f76ad85
ekoheryanto <eko.heryanto94@gmail.com>
2018-09-15 19:45:17 +0100

    tambah fungsi unshift dan pencarian index array

---
+++ array-indexof.js
@@ -0,0 +1,6 @@
+function callindex(){
+var data=['Cianjur','Bandung','Tasikmalaya','Bogor'];
+
+return data.indexOf('Medan');
+}
+console.log(callindex());
---
+++ array-unshift.js
@@ -0,0 +1,8 @@
+function callunshift(){
+var data = [23,24,25,26,27,28];
+console.log(data)
+data.unshift(21,22);
+
+return data;
+}
+console.log(callunshift());

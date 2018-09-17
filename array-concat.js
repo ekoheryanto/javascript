aeac6e5752c4abcba32ba6e2f66d8c0354a30a2f
ekoheryanto <eko.heryanto94@gmail.com>
2018-09-17 20:39:48 +0100

    edit file splice & add file array-concat

---
+++ array-concat.js
@@ -0,0 +1,9 @@
+function callconcat(){
+var data1=["Bandung","Cianjur","Bogor","Bekasi"];
+var data2=["Yogya","Solo","Magelang","Surabaya"];
+
+var gabung = data1.concat(data2);
+return gabung;
+
+}
+console.log(callconcat());
--- array-splice.js
+++ array-splice.js
@@ -1,8 +1,8 @@
 function callsplice() {
 var kota=['jakarta','medan','malang','cianjur'];
 console.log(kota)
-//kota.splice(2,0,'bandung');    //tambah penyisipan data
-kota.splice(3,1); //penghapusan data array
+//kota.splice(2,1,'bandung');    //tambah penyisipan data
+kota.splice(2,2); //penghapusan data array
 return kota;
 }
 console.log(callsplice());

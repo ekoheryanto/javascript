bfbf7a60f4abfa904259ce418d0f04d80ccdb865
ekoheryanto <eko.heryanto94@gmail.com>
2018-09-18 20:27:32 +0100

    edit file array concat

--- array-concat.js
+++ array-concat.js
@@ -1,8 +1,9 @@
 function callconcat(){
 var data1=["Bandung","Cianjur","Bogor","Bekasi"];
 var data2=["Yogya","Solo","Magelang","Surabaya"];
+var data3=["Taman Sari","Prambanan","Borobudur","Malioboro"];
 
-var gabung = data1.concat(data2);
+var gabung = data1.concat(data2,data3);
 return gabung;
 
 }
--- array-splice.js
+++ array-splice.js
@@ -2,7 +2,7 @@ function callsplice() {
 var kota=['jakarta','medan','malang','cianjur'];
 console.log(kota)
 //kota.splice(2,1,'bandung');    //tambah penyisipan data
-kota.splice(2,2); //penghapusan data array
+kota.splice(2,1); //penghapusan data array
 return kota;
 }
 console.log(callsplice());

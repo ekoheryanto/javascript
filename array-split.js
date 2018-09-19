28a654e0649ba261af3f94fb709d3402cccd128f
ekoheryanto <eko.heryanto94@gmail.com>
2018-09-19 21:31:36 +0100

    add finction array split

---
+++ array-split.js
@@ -0,0 +1,7 @@
+function callsplit(){
+var kalimat = "Halo coder semua mari kita belajar bersama";
+console.log(kalimat)
+var result = kalimat.split(" ");
+return result;
+}
+console.log(callsplit());

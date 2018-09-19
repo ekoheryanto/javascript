86b7952f4de0c6f6e04f788bd0074d2b92d1a407
ekoheryanto <eko.heryanto94@gmail.com>
2018-09-19 21:50:32 +0100

    add fungsi join dan reverse(membalik posisi di array)

---
+++ array-join.js
@@ -0,0 +1,8 @@
+function calljoin(){
+var kata =["aku","cinta","coding"];
+console.log(kata)
+var result=kata.join(" ");
+return result;
+
+}
+console.log(calljoin());
---
+++ array-reverse.js
@@ -0,0 +1,9 @@
+function callreverse(){
+var kartun =["doraemon","spongebob","ninja hatori","jiban"];
+console.log(kartun)
+return kartun.reverse();
+
+// var result =kartun.reverse();
+// return result;
+}
+console.log(callreverse());

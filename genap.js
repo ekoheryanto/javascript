60ac5be1fe2e59e5e41dc3af2353c63112b8f238
ekoheryanto <eko.heryanto94@gmail.com>
2018-09-24 21:41:45 +0700

    add file bilangan

---
+++ genap.js
@@ -0,0 +1,10 @@
+function callgenap(){
+var bilangan= []
+for (var i = 1; i < 10; i++) {
+  if(1%2 === 0){
+    bilangan.push(1)
+  }
+}
+return bilangan 
+}
+console.log(callgenap());
---
+++ modulus.js
@@ -0,0 +1,6 @@
+function callmodulus(){
+var a1 = 25;
+var a2 = 4;
+return a1 % a2;
+}
+console.log(callmodulus());

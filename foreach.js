7526eaa494ef03e082e6baa0b3e38b3746c1435c
ekoheryanto <eko.heryanto94@gmail.com>
2018-09-30 19:03:08 +0700

    add looping dengan forEach

---
+++ foreach.js
@@ -0,0 +1,9 @@
+function callforEach(){
+var data = ['asep','budi','dani'];
+data.forEach(function(item, index, array){
+console.log(item)
+console.log(index)
+console.log(array)
+})
+}
+callforEach();

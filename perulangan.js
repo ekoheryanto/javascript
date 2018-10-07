e5219391edac6b7519704b40cd6968322b98258d
ekoheryanto <eko.heryanto94@gmail.com>
2018-10-07 18:59:59 +0700

    add perulangan

---
+++ perulangan.js
@@ -0,0 +1,9 @@
+function callulang(){
+var mengulang=['satu','dua','tiga','empat','lima']
+for (var i = 0; i < mengulang.length; i++) {
+  console.log(mengulang[i])
+  console.log(i)
+  console.log(mengulang)
+}
+}
+callulang();

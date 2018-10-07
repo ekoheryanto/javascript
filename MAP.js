44ce5aec883bdb907605a41e366ae4e446a03d73
ekoheryanto <eko.heryanto94@gmail.com>
2018-10-07 18:47:40 +0700

    add file map

---
+++ MAP.js
@@ -0,0 +1,10 @@
+function callmap(){
+var hewan = ['kuda','keledai','singa'];
+hewan.map((namahewan, indexnya, array) => {
+console.log(namahewan)
+console.log(indexnya)
+console.log(array)
+
+})
+}
+callmap();
--- foreach.js
+++ foreach.js
@@ -3,7 +3,7 @@ var data = ['asep','budi','dani'];
 data.forEach(function(item, index, array){
 console.log(item)
 console.log(index)
-console.log(array)
+// console.log(array)
 })
 }
 callforEach();

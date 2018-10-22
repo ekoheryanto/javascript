101f65a0937ff82f20867bc944626c1ef5f8b67e
ekoheryanto <eko.heryanto94@gmail.com>
2018-10-22 18:58:41 +0700

    add file reguler expretion untuk serach data string dan match

---
+++ regexp-flag_G.js
@@ -0,0 +1,7 @@
+function callflag(){
+let str = "qwertyuiop-qwertyuiop"
+
+return str.match(/e/g)
+
+}
+console.log(callflag())
---
+++ regexp.js
@@ -0,0 +1,8 @@
+function callregexp(){
+let str = "abscedrfhdjljjns"
+
+console.log(str.search(/f/))
+console.log(str.search(/F/))
+console.log(str.search(/F/i))
+}
+callregexp()
